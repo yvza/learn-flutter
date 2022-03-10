4aec15d021ce5658c61d801d3a7d305d6c814055
yuza <akhmadyuza@gmail.com>
2022-03-10 21:42:49 +0700

    codelab 3

---
+++ lib/model/tourism_place.dart
@@ -0,0 +1,178 @@
+class TourismPlace {
+  String name;
+  String location;
+  String description;
+  String openDays;
+  String openTime;
+  String ticketPrice;
+  String imageAsset;
+  List<String> imageUrl;
+
+  TourismPlace({
+    required this.name,
+    required this.location,
+    required this.description,
+    required this.openDays,
+    required this.openTime,
+    required this.ticketPrice,
+    required this.imageAsset,
+    required this.imageUrl,
+  });
+}
+
+var tourismPlaceList = [
+  TourismPlace(
+      name: 'Chillon Castle',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Rhein Waterfall',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Mount Pilatus',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Jungfraujoch',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Jungfrau',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Zurich Lake',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Titlis',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Rigi',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Eiger',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Bernina Express',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+  TourismPlace(
+      name: 'Alets Glacier',
+      location: 'Switzerland',
+      description:
+          'Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya Apa ya',
+      openDays: 'Isuk smpek Bengi',
+      openTime: 'Jam pitu',
+      ticketPrice: 'Sewu',
+      imageAsset: 'https://random.imagecdn.app/500/350',
+      imageUrl: [
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350',
+        'https://random.imagecdn.app/500/350'
+      ]),
+];
