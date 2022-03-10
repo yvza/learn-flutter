75c1f96e60551a16bc4002b03c76ab9bc574719b
yuza <akhmadyuza@gmail.com>
2022-03-10 21:43:20 +0700

    codelab 3

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,8 +1,15 @@
+import 'dart:developer';
+
+import 'package:first_flutter_test/model/tourism_place.dart';
 import 'package:flutter/material.dart';
 
 // Codelab 1
 class DetailScreen extends StatelessWidget {
   var informationStyle = TextStyle(fontFamily: 'Oxygen');
+
+  final TourismPlace place;
+
+  DetailScreen({required this.place});
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +19,26 @@ class DetailScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Image.asset('images/top.jpg'),
+            SafeArea(
+              child: Stack(
+                children: <Widget>[
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back)),
+                  Padding(
+                    padding: EdgeInsets.only(left: 90.0),
+                    child: FavoriteButton(),
+                  )
+                ],
+              ),
+            ),
+            Image.network(place.imageAsset),
             Container(
               margin: EdgeInsets.only(top: 16.0),
               child: Text(
-                'Best Switzerland View',
+                place.name,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     fontSize: 30.0,
@@ -34,7 +56,7 @@ class DetailScreen extends StatelessWidget {
                       Icon(Icons.calendar_today),
                       SizedBox(height: 8.0),
                       Text(
-                        'We Are Open',
+                        place.openDays,
                         style: informationStyle,
                       ),
                     ],
@@ -43,14 +65,14 @@ class DetailScreen extends StatelessWidget {
                     children: <Widget>[
                       Icon(Icons.price_check),
                       SizedBox(height: 8.0),
-                      Text('Cheaper than last year!', style: informationStyle),
+                      Text(place.openTime, style: informationStyle),
                     ],
                   ),
                   Column(
                     children: <Widget>[
                       Icon(Icons.animation),
                       SizedBox(height: 8.0),
-                      Text('Astrazeneca Accepted', style: informationStyle),
+                      Text(place.ticketPrice, style: informationStyle),
                     ],
                   ),
                 ],
@@ -59,49 +81,83 @@ class DetailScreen extends StatelessWidget {
             Container(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                'Switzerland berlokasi di pusat Eropa Barat dianugerahi tidak hanya keindahan alamnya juga kekayaan budaya dan taraf hidup yang tinggi. Negara yang memiliki nama lengkap Konfederasi Swiss (Switzerland Confederation) ini merupakan negara yang menganut sistem pemerintahan  Republik Federal yang kepala negara dan kepala pemerintahannya adalah Presiden. Presiden Swiss dijabat secara bergantian oleh 7 menteri utama dengan masing-masing masa jabatannya selama 1 tahun. Ketujuh menteri utama tersebut dipilih oleh Majelis Federal (Federal Assembly). Ibukota negara Swiss adalah Kota Bern. Swiss memiliki luas wilayah sebesar 41.277 km2 dengan jumlah penduduk sebanyak 8.236.303 jiwa (estimasi Juli 2017). Swiss memiliki 4 bahasa resmi yaitu bahasa Jerman, bahasa Perancis, bahasa Italia dan bahasa Romande. Hal ini dikarenakan Swiss terdiri dari 4 etnis utama yaitu etnis Jerman, Perancis, Italia dan Romande. Penduduk Swiss mayoritas memeluk agama Katolik Roma (37,3%) dan  Protestan (24,9%).',
+                place.description,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
+                style: TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
               ),
             ),
             Container(
               height: 150,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
+                children: place.imageUrl.map((url) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
                     child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
-                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(url),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: Image.network(
-                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg',
-                      ),
-                    ),
-                  ),
-                ],
+                  );
+                }).toList(),
+                // children: [
+                //   Padding(
+                //     padding: const EdgeInsets.all(5.0),
+                //     child: ClipRRect(
+                //       borderRadius: BorderRadius.all(Radius.circular(10)),
+                //       child: Image.network(
+                //         'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
+                //       ),
+                //     ),
+                //   ),
+                //   Padding(
+                //     padding: const EdgeInsets.all(5.0),
+                //     child: ClipRRect(
+                //       borderRadius: BorderRadius.all(Radius.circular(10)),
+                //       child: Image.network(
+                //         'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
+                //       ),
+                //     ),
+                //   ),
+                //   Padding(
+                //     padding: const EdgeInsets.all(5.0),
+                //     child: ClipRRect(
+                //       borderRadius: BorderRadius.all(Radius.circular(10)),
+                //       child: Image.network(
+                //         'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg',
+                //       ),
+                //     ),
+                //   ),
+                // ],
               ),
             )
           ],
         ),
       )),
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+        print(isFavorite);
+      },
     );
   }
 }
